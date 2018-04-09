9ddffe6883bb718ee000c526220310ab63c145ff
15110123 <leminhtanvatc@yahoo.com.vn>
2018-04-10 00:50:11 +0700

    Khởi tạo 3 project: main, API và supplier

---
+++ WebGuard/WebGuard.Supplier/ChromiumExtension.cs
@@ -0,0 +1,62 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebGuard.Supplier
+{
+    public static class ChromiumExtension
+    {
+        public static async Task<ChromiumWebBrowser> ScrollNextViewHeight(this ChromiumWebBrowser brw)
+        {
+            var height = (await brw.EvaluateScriptAsync("window.innerHeight")).Result.ToString();
+            var res = await brw.EvaluateScriptAsync($"window.scrollTo(0, window.scrollY + {height});");
+            return brw;
+        }
+
+        public static async Task<ChromiumWebBrowser> WhileIsNotEndScroll(this ChromiumWebBrowser brw, Action action)
+        {
+            while (!await brw.IsEndScroll())
+            {
+                action.Invoke();
+                await Task.Delay(2000);
+            }
+            return brw;
+        }
+
+        private static async Task<bool> IsEndScroll(this IWebBrowser brw)
+        {
+            //Window height (viewable)
+            var height = int.Parse((await brw.EvaluateScriptAsync("window.innerHeight")).Result.ToString());
+
+            //Scroll height (viewable + non-viewable)
+            var scrollHeight = int.Parse((await brw.EvaluateScriptAsync("document.body.scrollHeight")).Result.ToString());
+
+            //Scroll position Y-axis
+            var curScrollPos = int.Parse((await brw.EvaluateScriptAsync("window.scrollY")).Result.ToString());
+
+            //Not 100% correct, '1' is for rounding value
+            return height + curScrollPos + 1 > scrollHeight;
+        }
+
+        public static Bitmap TakeScreenshot(this ChromiumWebBrowser brw, Form brwForm)
+        {
+            var printscreen = new Bitmap(brw.Width, brw.Height);
+            var gp = Graphics.FromImage(printscreen);
+            brw.Invoke((Action) (() =>
+            {
+                gp.CopyFromScreen(brw.PointToScreen(new Point(0, 0)), new Point(0, 0), new Size(brw.Width, brw.Height));
+            }));
+            return printscreen;
+        }
+
+        public static ChromiumWebBrowser To(this Bitmap bitmap, IList<Bitmap> bitmaps, ChromiumWebBrowser brw)
+        {
+            bitmaps.Add(bitmap);
+            return brw;
+        }
+    }
+}
---
+++ WebGuard/WebGuard.Supplier/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using CefSharp;
+using CefSharp.WinForms;
+using System.Windows.Forms;
+
+namespace WebGuard.Supplier
+{
+    public partial class MainForm : Form
+    {
+        private readonly List<Bitmap> _screenshots = new List<Bitmap>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeChromium("vnexpress.net").LoadingStateChanged += async (o, e) =>
+            {
+                if (e.IsLoading || !(o is ChromiumWebBrowser browser)) return;
+                await browser.WhileIsNotEndScroll(
+                    async () =>
+                    {
+                        await browser.TakeScreenshot(this)
+                            .To(_screenshots, browser)
+                            .ScrollNextViewHeight();
+                    }
+                    );
+                Invoke((Action) (() =>
+                {
+                    var frm = new Form();
+                    var ptb = new PictureBox
+                    {
+                        Image = _screenshots[0],
+                        Dock = DockStyle.Fill
+                    };
+                    frm.Controls.Add(ptb);
+                    frm.Show();
+                    //Close();
+                }));
+            };
+        }
+
+        public ChromiumWebBrowser InitializeChromium(string url)
+        {
+            var settings = new CefSettings
+            {
+                BrowserSubprocessPath = @"x86\CefSharp.BrowserSubprocess.exe",
+                UncaughtExceptionStackSize = 0,
+                LogSeverity = LogSeverity.Disable
+            };
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            var chromeBrowser = new ChromiumWebBrowser(url)
+            {
+                Dock = DockStyle.Fill
+            };
+            Controls.Add(chromeBrowser);
+            return chromeBrowser;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
